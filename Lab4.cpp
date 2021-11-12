067f78a0e8cce1448277ef7c4c69cf213e05dd18
TProctor-root <57115882+TProctor-root@users.noreply.github.com>
2021-11-12 11:28:55 -0800

    Update Lab4.cpp

--- Lab4.cpp
+++ Lab4.cpp
@@ -1,14 +1,3 @@
-//============================================================================
-// Name        : Lab4.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
-//Name: Tristin Proctor
-//UCINetID: tproctor
-//Description: Starts the program
-
 #include "FileHandling.h"
 #include <iostream>
 using namespace std;
